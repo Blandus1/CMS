123389aed60485b34499ecd95c76a201a637c845
Blandus1 <blandine0045@gmail.com>
2026-04-28 17:29:11 +0200

    added a _redirects file in public

--- cms-project/src/App.jsx
+++ cms-project/src/App.jsx
@@ -1,17 +1,23 @@
+import { BrowserRouter,Routes, Route, Link } from 'react-router-dom'
 import './App.css'
 import Cmss from './cms2'
 import Cms from './cms'
-
 
 function App() {
  
 
   return (
     <>
- 
   {/* <Cmss/> */}
-    <Cms/>
-
+  <BrowserRouter>
+  {/* <Link to="/" className='uppercase text-red-400'>Mine</Link>
+  <Link to="version2" className='uppercase'>v2</Link> */}
+  <Routes>
+    <Route path="/" element=  {<Cms/>}/>
+    <Route path="/version2" element=  {<Cmss/>}/>
+  </Routes>
+  </BrowserRouter>
+  
     </>
   )
 }
